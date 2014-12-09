e2ea48d12eb960162a4dcd6ecfddc0382b0a69ba
jredd46 <jredd46@gmail.com>
2014-12-09 05:23:34 -0600

    got the ui mostly working... there are some issues with the asset page still that need to be resolved. and https is now default communiction

--- redd/js/app.js
+++ redd/js/app.js
@@ -1,25 +1,131 @@
 App = Ember.Application.create();
 
-// Models
 
+
+// Models
+App.Project = DS.Model.extend({
+  name: DS.attr('string'),
+  date_created: DS.attr('date'),
+  created_by: DS.attr('string'),
+  is_active: DS.attr('boolean')
+});
+
+App.Department = DS.Model.extend({
+  name: DS.attr('string'),
+  date_created: DS.attr('date'),
+  created_by: DS.attr('string'),
+  is_active: DS.attr('boolean'),
+  project: DS.hasMany('project', {async: true})
+});
+
+App.Asset = DS.Model.extend({
+  name: DS.attr('string'),
+  date_created: DS.attr('date'),
+  created_by: DS.attr('string'),
+  project: DS.belongsTo('project', {async: true}),
+  current_department: DS.hasMany('department', {async: true})
+});
+
+// AUTHORIZATION HEADERS
+App.AuthorizationAdapter = DS.RESTAdapter.extend({
+  host: 'https://localhost:6080',
+  headers: function(){
+    return {'AUTHORIZATION': "Token "+ $.cookie('token')}
+  }.property().volatile(),
+
+  buildURL: function() {
+    var normalURL = this._super.apply(this, arguments);
+    // Django adds '/' to urls if it doesn't have one. It will break CORS
+    return normalURL + '/';
+  },
+  updateRecord: function(store, type, record) {
+    var get = Ember.get;
+    var data = {};
+    var model_type = type.typeKey;
+    var serializer = store.serializerFor(model_type);
+
+    serializer.serializeIntoHash(data, type, record);
+    var id = get(record, 'id');
+    // Send the root data without the models name back to django
+    return this.ajax(this.buildURL(model_type, id, record), "PUT", { data: data[model_type] });
+  },
+  createRecord: function(store, type, record) {
+    var data = {};
+    var model_type = type.typeKey
+    var serializer = store.serializerFor(model_type);
+
+    serializer.serializeIntoHash(data, type, record, { includeId: true });
+
+    return this.ajax(this.buildURL(model_type, null, record), "POST", { data: data[model_type] });
+  },
+  actions: {
+    error: function(reason, transition) {
+
+      if (reason.status === 401) {
+        this.redirect_to_login(transition);
+      }else{
+        console.log(reason, transition)
+        alert('something went wrong');
+      }
+    }
+  }
+});
+
+
+// NAMESPACE ADAPTERS
+App.CoreAdapter = App.AuthorizationAdapter.extend({
+  namespace: 'core'
+});
+App.TrackerAdapter = App.AuthorizationAdapter.extend({
+  namespace: 'tracker'
+});
+App.ProjectAdapter = App.CoreAdapter;
+App.DepartmentAdapter = App.CoreAdapter;
+App.AssetAdapter = App.TrackerAdapter;
+
+// SERIALIZERS
+App.ProjectSerializer = DS.RESTSerializer.extend({
+  normalizePayload: function(payload) {
+    return {project: payload};
+  }
+});
+App.DepartmentSerializer = DS.RESTSerializer.extend({
+  normalizePayload: function(payload) {
+    return {department: payload};
+  }
+});
+App.AssetSerializer = DS.RESTSerializer.extend({
+  normalizePayload: function(payload) {
+    return {asset: payload};
+  }
+});
 // Routes
 App.Router.map(function() {
+  //this.resource('projects', function() {
+  //  this.resource('create');
+  //});
   this.resource('projects');
-  this.resource('assets');
+  this.resource('project', { path: '/project/:project_id'});
+  this.resource('project-create', { path: 'projects/create'});
   this.resource('departments');
-  this.resource('login');
+  this.resource('department', { path: '/department/:department_id'});
+  this.route('department-create', { path: 'department/create'});
+  this.route('assets');
+  this.route('asset', { path: '/asset/:asset_id'});
+  this.route('asset-create', { path: '/asset/create'});
+  this.route('login');
 });
 
 App.ApplicationRoute = Ember.Route.extend({
   actions: {
     // create a global logout action
     logout: function() {
-      console.log('hit the route')
       // get the sessions controller instance and reset it to then transition to the sessions route
-      this.controllerFor('login').set('token', null)
-      //this.controllerFor('login').reset();
+      this.controllerFor('login').set('token', null);
       $.removeCookie('token', { expires:.25, path: '/' });
-      //this.controllerFor('login').get();
+      this.controllerFor('login').reset();
+      this.controllerFor('login').set('user_name', null);
+      $.removeCookie('user_name', { expires:.25, path: '/' });
       this.transitionTo('login');
     }
   }
@@ -31,27 +137,18 @@ App.AuthenticatedRoute = Ember.Route.extend({
       this.redirect_to_login(transition)
     }
   },
-
   redirect_to_login: function(transition) {
     var loginController = this.controllerFor('login');
     loginController.set('attemptedTransition', transition);
     this.transitionTo('login');
   },
-
-  get_json_with_token: function(url){
-    var token = this.controllerFor('login').get('token');
-    $.ajaxSetup({
-      headers : {"AUTHORIZATION": "Token "+token}
-    });
-
-    return $.getJSON(url);
-  },
-
   actions: {
     error: function(reason, transition) {
+
       if (reason.status === 401) {
         this.redirect_to_login(transition);
       }else{
+        console.log(reason, transition)
         alert('something went wrong');
       }
     }
@@ -60,19 +157,65 @@ App.AuthenticatedRoute = Ember.Route.extend({
 
 App.ProjectsRoute = App.AuthenticatedRoute.extend({
   model: function() {
-    return this.get_json_with_token('http://127.0.0.1:8000/core/project-list/');
+    return this.store.findAll('project').then(function(data) {
+      return data;
+    });
   }
 });
 
+App.ProjectRoute = App.AuthenticatedRoute.extend({
+  model: function(params) {
+    return this.store.find('project', params.project_id).then(function(data) {
+      return data
+    });
+  }
+});
+
+App.ProjectCreateRoute = App.AuthenticatedRoute.extend({});
+
 App.DepartmentsRoute = App.AuthenticatedRoute.extend({
   model: function() {
-    return this.get_json_with_token('http://127.0.0.1:8000/core/department-list/');
+    return this.store.findAll('department').then(function(data) {
+      return data
+    });
+  }
+});
+
+App.DepartmentRoute = App.AuthenticatedRoute.extend({
+  model: function(params) {
+    return this.store.find('department', params.department_id).then(function(data) {
+      return data
+    });
   }
 });
 
 App.AssetsRoute = App.AuthenticatedRoute.extend({
   model: function() {
-    return this.get_json_with_token('http://127.0.0.1:8000/tracker/asset-list/');
+    return this.store.findAll('asset').then(function(data) {
+      return data
+    });
+  }
+
+});
+
+App.AssetRoute = App.AuthenticatedRoute.extend({
+  model: function(params) {
+    return this.store.find('asset', params.asset_id).then(function(data) {
+      return data
+    });
+  }
+});
+
+App.AssetCreateRoute = App.AuthenticatedRoute.extend({
+  model: function() {
+    var new_asset = this.store.createRecord('asset')
+
+    return this.store.createRecord('asset');
+  },
+  setupController: function(controller, model) {
+    controller.set('department', this.store.findAll('department'));
+    controller.set("model", model);
+    controller.set('project', this.store.findAll('project'));
   }
 });
 
@@ -81,12 +224,211 @@ App.LoginRoute = Ember.Route.extend({
     controller.reset();
   }
 });
-
 App.IndexRoute = App.AuthenticatedRoute.extend();
 
+// CONTROLLERS
+App.AssetsController = Ember.ArrayController.extend({
+  total_assets: function() {
+    return this.get('model.length');
+  }.property('@each')
+});
 
+App.AssetController = Ember.ObjectController.extend({
+  asset_creation_failed: false,
+  error_message: null,
+  is_editing: false,
+  actions: {
+    edit: function () {
+      this.set('is_editing', true);
+    },
+    update: function () {
+      this.get('model').save();
+      this.set('is_editing', false)
+    },
+    delete_model: function () {
+      var project = this.get('model')
+      project.deleteRecord();
+      project.save().then(function (resp) {
+        this.transitionToRoute('assets');
+      }.bind(this), function (resp) {
+        if (resp.status === 400) {
+          this.set("error_message", resp.responseText);
+        } else {
+          console.log(resp);
+          alert('an error occured communicating with the server.')
+        }
+        this.set("asset_creation_failed", true);
+      }.bind(this));
+      this.set('is_editing', false)
+    },
+    cancel_edit: function () {
+      this.get('model').rollback();
+      this.set('is_editing', false)
+    }
+  }
+});
 
-App.ProjectsController = Ember.Controller.extend({
+App.AssetCreateController = Ember.ObjectController.extend({
+  //username: this.controllerFor('login').get('user_name'),
+  needs: ['login'],
+  project: null,
+  department: null,
+  selected_project: null,
+  actions: {
+    create_asset: function() {
+      this.set('created_by', this.get('controllers.login').get('user_name'));
+      this.set('date_created', new Date());
+      this.get('model').save().then(function(resp){
+        console.log('success!', resp);
+        this.transitionToRoute('assets');
+      }.bind(this), function(resp){
+        if (resp.status === 400){
+          this.set("error_message", resp.responseText);
+        }else {
+          console.log(resp);
+          alert('an error occurred communicating with the server.')
+        }
+        this.set("department_creation_failed", true);
+      }.bind(this));
+    }
+  }
+});
+
+App.DepartmentsController = Ember.ArrayController.extend({
+  total_departments: function() {
+    return this.get('model.length');
+  }.property('@each')
+});
+
+App.DepartmentController = Ember.ObjectController.extend({
+  department_creation_failed: false,
+  error_message: null,
+  is_editing: false,
+  actions: {
+    edit: function () {
+      this.set('is_editing', true);
+    },
+    update: function () {
+      this.get('model').save();
+      this.set('is_editing', false)
+    },
+    delete_model: function () {
+      console.log('in delete')
+      var project = this.get('model')
+      project.deleteRecord();
+      project.save().then(function (resp) {
+        this.transitionToRoute('departments');
+      }.bind(this), function (resp) {
+        if (resp.status === 400) {
+          this.set("error_message", resp.responseText);
+        } else {
+          console.log(resp);
+          alert('an error occured communicating with the server.')
+        }
+        this.set("project_creation_failed", true);
+      }.bind(this));
+      this.set('is_editing', false)
+    },
+    cancel_edit: function () {
+      this.get('model').rollback();
+      this.set('is_editing', false)
+    }
+  }
+});
+
+App.DepartmentCreateController = Ember.ArrayController.extend({
+  //username: this.controllerFor('login').get('user_name'),
+  name: null,
+  actions: {
+    create_department: function() {
+      var data = this.getProperties('name', 'is_active');
+      data['created_by'] = this.controllerFor('login').get('user_name')
+      data['date_created'] = new Date()
+      //console.log(this.get('model').create())
+      var department = this.store.createRecord('department', data);
+      department.save().then(function(resp){
+        console.log(resp)
+        this.transitionTo('departments');
+      }.bind(this), function(resp){
+        if (resp.status === 400){
+          this.set("error_message", resp.responseText);
+        }else {
+          console.log(resp);
+          alert('an error occured communicating with the server.')
+        }
+        this.set("department_creation_failed", true);
+      }.bind(this));
+    }
+  }
+});
+
+App.ProjectsController = Ember.ArrayController.extend({
+  total_projects: function() {
+    return this.get('model.length');
+  }.property('@each')
+});
+
+App.ProjectController = Ember.ObjectController.extend({
+  is_editing: false,
+  actions: {
+    edit: function() {
+      this.set('is_editing', true);
+    },
+    update: function(){
+      this.get('model').save();
+      this.set('is_editing', false)
+    },
+    delete_model: function(){
+      console.log('in delete')
+      var project = this.get('model')
+      project.deleteRecord();
+      project.save().then(function(resp){
+        this.transitionToRoute('projects');
+      }.bind(this), function(resp){
+        if (resp.status === 400){
+          this.set("error_message", resp.responseText);
+        }else {
+          console.log(resp);
+          alert('an error occured communicating with the server.')
+        }
+        this.set("project_creation_failed", true);
+      }.bind(this));
+      //this.set('is_editing', false)
+    },
+    cancel_edit: function() {
+      this.get('model').rollback();
+      this.set('is_editing', false)
+    }
+  }
+});
+
+App.ProjectCreateController = Ember.ArrayController.extend({
+  //username: this.controllerFor('login').get('user_name'),
+  project_creation_failed: false,
+  error_message: null,
+  name: null,
+  is_active: false,
+
+  actions: {
+    create_project: function() {
+      var data = this.getProperties('name', 'is_active');
+      data['created_by'] = this.controllerFor('login').get('user_name')
+      data['date_created'] = new Date()
+      //console.log(this.get('model').create())
+      var project = this.store.createRecord('project', data);
+      project.save().then(function(resp){
+        this.transitionTo('projects');
+      }.bind(this), function(resp){
+        if (resp.status === 400){
+          this.set("error_message", resp.responseText);
+        }else {
+          console.log(resp);
+          alert('an error occured communicating with the server.')
+        }
+        this.set("project_creation_failed", true);
+      }.bind(this));
+    }
+  }
 });
 
 App.LoginController = Ember.Controller.extend({
@@ -98,12 +440,13 @@ App.LoginController = Ember.Controller.extend({
 
   token: $.cookie('token'),
   token_changed: function() {
-    //localStorage.token = this.get('token');
-    console.log('token changed', this.get('token'))
     $.cookie('token', this.get('token'), { expires:.25, path: '/' });
-
-    //$.cookie('token', this.get('token'), {expires:.1, path: '/'});
   }.observes('token'),
+
+  user_name: $.cookie('user_name'),
+  user_name_changed: function() {
+    $.cookie('user_name', this.get('user_name'), { expires:.25, path: '/' });
+  }.observes('user_name'),
 
   actions: {
     login: function() {
@@ -113,17 +456,19 @@ App.LoginController = Ember.Controller.extend({
         isProcessing: true
       });
       // Gather and post the authentication information
+      this.set('user_name', this.get('username'));
       var data = this.getProperties('username', 'password');
       this.set("timeout", setTimeout(this.slowConnection.bind(this), 5000));
-      var request = Ember.$.post('http://127.0.0.1:8000/api-token-auth/', data);
+
+      //var request=$.getJSON('http://localhost:9080/api-token-auth/');
+      var request = jQuery.post('https://localhost:6080/api-token-auth/', data);
       request.then(this.success.bind(this), this.failure.bind(this));
     }
   },
-  success: function(response) {
-    //console.log('shit was successful', response);
-    this.reset();
-    this.set('token', response.token);
 
+  success: function(response) {
+    this.reset(),
+    this.set('token', response.token);
     // Check to see if redirected from different route
     var attemptedTransition = this.get('attemptedTransition');
     if (attemptedTransition) {
@@ -136,9 +481,11 @@ App.LoginController = Ember.Controller.extend({
   },
 
   failure: function(response) {
+    this.set("user_name", null)
     this.reset();
+    console.log('shit broke')
     // Detect the error type and response with appropriate message
-    console.log(response)
+    console.log(response);
     if (response.status === 400){
       this.set("error_message", 'Invalid username or password.');
     }else if (response.status === 0){
@@ -163,4 +510,8 @@ App.LoginController = Ember.Controller.extend({
       is_slow_connection: false
     })
   }
+});
+
+Ember.Handlebars.helper('format-date', function(date) {
+  return moment(date).fromNow();
 });
