d5806cd031b34e50ec1354f4210159b5fd649838
jredd <jredd46@gmail.com>
2016-04-08 00:53:48 -0500

    adding the routes and views needed for the components

---
+++ app/adapters/asset.js
@@ -0,0 +1,13 @@
+import Ember from 'ember';
+import DRFAdapter from './drf';
+
+export default DRFAdapter.extend({
+  host: 'http://104.236.96.7',
+  session: Ember.inject.service('session'),
+  headers: Ember.computed('session.data.authenticated.access_token', function() {
+    return {
+      'Authorization': 'JWT '+ this.get('session.data.authenticated.token'),
+      'Content-type': 'application/json'
+    };
+  })
+});
