f915319e5db90f5ee01d132887f322165e8f212f
jredd <jredd46@gmail.com>
2016-05-08 23:44:12 -0500

    added in destroying of a project. Working on trying to modify the departments a project has... it's difficult

--- app/components/details-departments.js
+++ app/components/details-departments.js
@@ -1,4 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  edit: false,
+  actions: {
+    edit: function() {
+      this.toggleProperty('edit');
+      console.log(this.get('edit'));
+    }
+  },
+  stuff: ['turtles', 'apples', 'boats']
 });
